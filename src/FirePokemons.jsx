41df07ea79668d2870b5f285b4e9937f58cf6ac4
BarBius <mnanikashvili27@gmail.com>
2024-04-04 12:03:47 +0200

    last

--- src/FirePokemons.jsx
+++ src/FirePokemons.jsx
@@ -1,11 +1,17 @@
+import React from 'react';
+import './App.css'; // Import the CSS file
+
 const FirePokemons = ({ pokemons }) => {
     const firePokemons = pokemons.filter(pokemon => pokemon.type === "Fire");
 
     return (
-        <div style={{ backgroundColor: 'orange' }}>
-            Fire Pokemons :
+        <div className="fire-container"> {/* Apply the fire-container class */}
+            <h2>Fire Pokemons</h2>
             {firePokemons.map((pokemon, index) => (
-                <h3 key={index}>{pokemon.name} <img src={pokemon.image} alt={pokemon.name} /></h3>
+                <div key={index} className="fire-pokemon"> {/* Apply the fire-pokemon class */}
+                    <h3 className="fire-name">{pokemon.name}</h3> {/* Apply the fire-name class */}
+                    <img className="fire-image" src={pokemon.image} alt={pokemon.name} /> {/* Apply the fire-image class */}
+                </div>
             ))}
         </div>
     );
--- src/LastCapturedPokemons.jsx
+++ src/LastCapturedPokemons.jsx
@@ -1,18 +1,21 @@
 const LastCapturedPokemons = ({ pokemons }) => {
-const lastCapturedPokemons = pokemons.slice(0, 3);
+    // Sort the pokemons array by capture date in descending order
+    const sortedPokemons = pokemons.sort((a, b) => new Date(b.capturedAt) - new Date(a.capturedAt));
+    // Take the last three elements from the sorted array
+    const lastCapturedPokemons = sortedPokemons.slice(0, 3);
 
-return (
-<div style={{ backgroundColor: 'lightgreen' }}>
-    LAST CAPTURED POKEMONS: 
-    {lastCapturedPokemons.map((pokemon) => (
-    <div key={pokemon.id}>
-        <p>{pokemon.name}</p>
-        <p>Capture Date: {pokemon.capturedAt}</p>
-        <img src={pokemon.image} alt={pokemon.name} />
-    </div>
-    ))}
-</div>
-);
+    return (
+        <div style={{ backgroundColor: 'lightgreen' }}>
+            LAST CAPTURED POKEMONS: 
+            {lastCapturedPokemons.map((pokemon) => (
+                <div key={pokemon.id}>
+                    <p>{pokemon.name}</p>
+                    <p>Capture Date: {pokemon.capturedAt}</p>
+                    <img src={pokemon.image} alt={pokemon.name} />
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default LastCapturedPokemons;
